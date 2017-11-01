04b0a072b1e037c893fafd467139acfe10d715b1
Jan Dahlke <jan.dahlke@pdfforge.org>
2017-11-01 17:12:05 +0100

    puppet songlist, stupid yet

---
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SongList extends Component{
+
+
+    render() {
+       return (
+           <div>
+               songlist goes here
+           </div>
+       );
+    }
+}
+
+export default SongList;
--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -4,12 +4,23 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client'; //completly agnostic to react/angular etc
 import {ApolloProvider} from 'react-apollo'; // here comes the react-wrapper "glue-layer"
 
+import SongList from './components/SongList';
+
+
+//------------------
+
 const client = new ApolloClient({});
 
 const Root = () => {
     return (
         <ApolloProvider client={client}>
-            <div>Lyrical</div>
+            <section className="container">
+
+                <h3>RANDOM HEADLINE</h3>
+
+                <SongList />
+
+            </section>
         </ApolloProvider>
     );
 };
