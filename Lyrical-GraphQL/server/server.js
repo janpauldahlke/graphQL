29a90018e7ed1426f198a109b57e53ffe0aff0f0
Jan Dahlke <jan.dahlke@pdfforge.org>
2017-11-01 14:28:17 +0100

    docker-compose mongobd and, update syntax on decprecated mongoose, switch port due double assignment

--- Lyrical-GraphQL/server/server.js
+++ Lyrical-GraphQL/server/server.js
@@ -8,13 +8,15 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb://stephen:stephen@ds021182.mlab.com:21182/lyricaldb';
+const MONGO_URI = 'mongodb://localhost:4000/LyricalDB';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {
+  useMongoClient: true
+});
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
