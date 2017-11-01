832d96814eb20b6dd0a7473265797e714ba970b9
Jan Dahlke <jan.dahlke@pdfforge.org>
2017-11-01 16:09:59 +0100

    add ApolloProvider and ApolloClient to Root

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ApolloClient from 'apollo-client'; //completly agnostic to react/angular etc
+import {ApolloProvider} from 'react-apollo'; // here comes the react-wrapper "glue-layer"
+
+const client = new ApolloClient({});
+
 const Root = () => {
-    return <div>Lyrical</div>
+    return (
+        <ApolloProvider client={client}>
+            <div>Lyrical</div>
+        </ApolloProvider>
+    );
 };
 
 ReactDOM.render(
     <Root />,
     document.querySelector('#root')
-)
+);
+
