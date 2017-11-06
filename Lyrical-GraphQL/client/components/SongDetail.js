beaf89cff1f360c4c09df25078d105fd2ecda032
Jan Dahlke <jan.dahlke@pdfforge.org>
2017-11-06 15:18:35 +0100

    dive deep again, after a mean peice of syntax on intercepting querys with variables

--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import graphlql from 'react-apollo';
-
+import {graphql} from 'react-apollo'; // import graphql from 'react-apollo'; will produce an error, care about this sh1t !!!
 import {Link} from 'react-router';
+
+import getSongQuery from '../queries/fetchSongByID';
 
 class SongDetail extends Component{
 
     render() {
 
         //let me see you naked
-        //console.log(this.props)
+        //console.log(this.props) -> check params object
 
         return (
             <div className="container">
@@ -21,6 +22,13 @@ class SongDetail extends Component{
     }
 }
 
-export default SongDetail;
+//export default SongDetail;
+//? how to intercept the query and pass it the query variable?
+// 2nd argument of query is an options object, that keeps
+//lol syntax is quite difficulte here, learn this shit eager
+
+export default graphql(getSongQuery, {
+    options: (props) => { return { variables : { id: props.params.id } } }
+})(SongDetail);
 
 
