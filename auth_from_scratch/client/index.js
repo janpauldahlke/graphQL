b1e8a79e365bebe87b72811468df9f5fed189656
janpauldahlke <janpauldahlke@gmail.com>
2017-11-09 08:52:42 +0100

    boilerplate client with apollo and react router

--- auth_from_scratch/client/index.js
+++ auth_from_scratch/client/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App.js';
+
+
+const client = new ApolloClient({
+    //dataIdFromObject : o => o.id
+});
 
 const Root = () => {
 
   console.log(this);
 
   return (
-    <div>
-      Auth Starter
-    </div>
+      <ApolloProvider client={client}>
+          <Router history={hashHistory}>
+              <Route path="/" component={App}>
+
+              </Route>
+          </Router>
+      </ApolloProvider>
   );
 };
 
